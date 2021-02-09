3e4f07ce8c486095460378d87d8c21e86927a257
gustavojra <gustavo.aroeira@gmail.com>
2021-02-09 17:09:53 -0500

    More features

--- src/TermSymbols.jl
+++ src/TermSymbols.jl
@@ -1,8 +1,11 @@
 module TermSymbols
 using Combinatorics
+import Base.*
+import Base.:(==)
 
 export Determinant
 export get_dets
+export one_det_wfn
 export latex_that_shit
 export latex
 
@@ -11,8 +14,8 @@ orb_per_shell = Dict(
         "p" => 3,
         "d" => 5,
         "f" => 7,
-        "h" => 9,
-        "g" => 11
+        "g" => 9,
+        "h" => 11
 )
 
 function num_of_orb(Orb::String)
@@ -31,6 +34,11 @@ struct Determinant
     Ms::Rational{Int64}
     α::Dict{String,BitArray{1}}
     β::Dict{String,BitArray{1}}
+end
+
+function ==(D1::Determinant, D2::Determinant)
+
+    return D1.Ml == D2.Ml && D1.Ms == D2.Ms && D1.α == D2.α && D1.β == D2.β
 end
 
 function Determinant(αstr::Dict{String,BitArray{1}}, βstr::Dict{String,BitArray{1}})
@@ -163,6 +171,29 @@ function latex(D::Determinant, core="")
     return out
 end
 
+function latex(Ds::Array{Determinant,1}, core="")
+
+    out = core
+
+    Ml_vals = [D.Ml for D = Ds]
+
+    p = sortperm(Ml_vals, rev=true)
+
+    for (i,D) in enumerate(Ds[p])
+
+        if i==1 || Ml_vals[p][i] != Ml_vals[p][i-1]
+            
+            out = out*"\n"*raw"\textasteriskcentered{} M$_l$ = "*"$(D.Ml) "*raw"M$_s$ = TODO "*"\n\n"
+
+        end
+
+        out = out*latex(D,core)
+        out = out*raw"\\\\"*"\n"
+    end
+
+    return out
+end
+
 function latex_that_shit(dets::Array{Determinant,1}, Ml::Int)
 
     dets = filter(d->d.Ml == Ml, dets)
@@ -193,4 +224,131 @@ function latex_that_shit(dets::Array{Determinant,1}, Ml::Int)
 
 end
 
+function combine_shells(A::Array{Determinant,1}, B::Array{Determinant,1})
+
+    out = Determinant[]
+
+    for i in eachindex(A)
+        for j in eachindex(B)
+            push!(out, A[i]*B[j])
+        end
+    end
+
+    return out
+end
+
+function get_dets(conf::String)
+    
+    r = r"\d\w\d+?"
+
+    shells_comb = Any[]
+
+    for slice in findall(r, conf)
+
+        possible_dets = Determinant[]
+
+        orb = conf[slice][1:2]
+        nelec = parse(Int, conf[slice][3:end])
+
+        Norb = num_of_orb(orb)
+
+        max_one_spin = min(Norb, nelec)
+        min_one_spin = nelec - max_one_spin
+
+        α = max_one_spin
+        β = min_one_spin
+
+        while β ≤ max_one_spin
+            possible_dets = vcat(possible_dets, get_dets(Dict(orb=>(α,β))))
+            α -= 1
+            β += 1
+        end
+
+        push!(shells_comb, possible_dets)
+    end
+
+    out = shells_comb[1]
+
+    for i = 2:length(shells_comb)
+        out = combine_shells(out, shells_comb[i])
+    end
+
+    return out
+end
+
+function *(D1::Determinant, D2::Determinant)
+    
+    Ml = D1.Ml + D2.Ml
+    Ms = D1.Ms + D2.Ms
+
+    α = merge(D1.α, D2.α)
+    β = merge(D1.β, D2.β)
+
+    return Determinant(Ml, Ms, α, β)
+
+end
+
+function one_det_wfn(dets::Array{Determinant,1})
+
+    types = [(D.Ml, D.Ms) for D = dets]
+
+    out = Determinant[]
+
+    for i = eachindex(dets)
+
+        D = dets[i]
+        ncases = findall(Mvals-> Mvals[1] == D.Ml && Mvals[2] == D.Ms, types)
+
+        if length(ncases) == 1
+            push!(out, D)
+        end
+    end
+
+    return out
+end
+
 end # module
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
